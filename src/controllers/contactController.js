a08c8a1f508c236885a9d555b4b5066516179993
avi_hr <avinash.hoskere18@gmail.com>
2024-07-08 20:28:14 +0530

    fixed primary to secondary change logic

--- src/controllers/contactController.js
+++ src/controllers/contactController.js
@@ -4,117 +4,93 @@ import Contact from "../models/contact.js";
 export const identifyContact = async (req, res) => {
     const { email, phoneNumber } = req.body;
 
-    let existingContact = await Contact.findOne({
+    let contacts = await Contact.findAll({
         where: {
-            email,
-            phoneNumber,
+            [Op.or]: [{ email }, { phoneNumber }],
         },
     });
-    if (!existingContact) {
-        let contacts = await Contact.findAll({
-            where: {
-                [Op.or]: [{ email }, { phoneNumber }],
-            },
-        });
 
-        console.log(
-            "------------------" +
-                JSON.stringify(contacts, null, 2) +
-                "------------------"
-        );
+    if (contacts.length === 0) {
+        let newContact = req.body;
+        newContact.linkPrecedence = "primary";
 
-        if (contacts.length === 0) {
-            const newContact = await Contact.create({
-                email,
-                phoneNumber,
-                linkPrecedence: "primary",
-            });
-
-            return res.status(200).json({
-                contact: {
-                    primaryContatctId: newContact.id,
-                    emails: [newContact.email],
-                    phoneNumbers: [newContact.phoneNumber],
-                    secondaryContactIds: [],
-                },
-            });
+        try {
+            newContact = await Contact.create(newContact);
+        } catch (err) {
+            console.log(err);
+            return res.status(500).send(err.message);
         }
-
-        let primaryContact = contacts.find(
-            (contact) => contact.linkPrecedence === "primary"
-        );
-        console.log("primaryContact", primaryContact);
-        if (!primaryContact) {
-            primaryContact = contacts[0];
-            if (!primaryContact.linkedId) {
-                primaryContact.linkPrecedence = "primary";
-                await primaryContact.save();
-            }
-        }
-
-        const secondaryContactIds = contacts
-            .filter((contact) => contact.linkPrecedence !== "primary")
-            .map((contact) => contact.id);
-
-        if (
-            contacts.some(
-                (contact) =>
-                    contact.email !== email ||
-                    contact.phoneNumber !== phoneNumber
-            )
-        ) {
-            console.log(
-                "----------------creating new secondary contact--------"
-            );
-            const newContact = await Contact.create({
-                email,
-                phoneNumber,
-                linkedId: primaryContact.linkedId || primaryContact.id,
-                linkPrecedence: "secondary",
-            });
-            secondaryContactIds.push(newContact.id);
-        }
-
-        let secondaryContacts = await Contact.findAll({
-            where: {
-                linkedId: primaryContact.id,
-            },
-        });
-
-        console.log(
-            "------------------secondary contacts",
-            JSON.stringify(secondaryContacts)
-        );
-
-        let emailArray = [
-            primaryContact.email,
-            ...contacts
-                .filter((contact) => primaryContact.email !== contact.email)
-                .map((contact) => Number(contact.email)),
-        ];
-
-        if (email && !emailArray.includes(email)) emailArray.push(email);
-
-        let phoneArray = [
-            Number(primaryContact.phoneNumber),
-            ...contacts
-                .filter(
-                    (contact) =>
-                        primaryContact.phoneNumber !== contact.phoneNumber
-                )
-                .map((contact) => Number(contact.phoneNumber)),
-        ];
-
-        if (phoneNumber && !phoneArray.includes(phoneNumber))
-            phoneArray.push(phoneNumber);
 
         return res.status(200).json({
             contact: {
-                primaryContatctId: primaryContact.linkedId || primaryContact.id,
-                emails: emailArray,
-                phoneNumbers: phoneArray,
-                secondaryContactIds,
+                primaryContatctId: newContact.id,
+                emails: [newContact.email],
+                phoneNumbers: [newContact.phoneNumber],
+                secondaryContactIds: [],
             },
         });
-    } else return res.status(200).json({ contact: existingContact });
+    }
+    let primaryContact = contacts.find(
+        (contact) => contact.linkPrecedence === "primary"
+    );
+    if (!primaryContact) {
+        let primaryContactId = contacts[0].linkedId;
+        primaryContact = Contact.findOne(primaryContactId);
+        contacts = Contact.findAll({
+            where: {
+                linkedId: primaryContactId,
+            },
+        });
+    }
+
+    const emails = new Set();
+    const phoneNumbers = new Set();
+    const secondaryContactIds = [];
+
+    contacts.forEach((contact) => {
+        if (
+            contact.linkPrecedence === "primary" &&
+            contact.id !== primaryContact.id
+        ) {
+            contact.linkPrecedence = "secondary";
+            contact.linkedId = primaryContact.id;
+            contact.save();
+        }
+        if (contact.email) emails.add(contact.email);
+        if (contact.phoneNumber) phoneNumbers.add(contact.phoneNumber);
+        if (contact.id !== primaryContact.id)
+            secondaryContactIds.push(contact.id);
+    });
+
+    // If the current data does not match any contact, create a new secondary contact
+
+    if (!emails.has(email) || !phoneNumbers.has(String(phoneNumber))) {
+        console.log("creating New contact");
+        let newContact;
+        try {
+            newContact = await Contact.create({
+                phoneNumber,
+                email,
+                linkPrecedence: "secondary",
+                linkedId: primaryContact.id,
+            });
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json({
+                message: "Internal server error",
+            });
+        }
+        secondaryContactIds.push(newContact.id);
+        if (newContact.email) emails.add(newContact.email);
+        if (newContact.phoneNumber) phoneNumbers.add(newContact.phoneNumber);
+    }
+
+    return res.status(200).json({
+        contact: {
+            primaryContatctId: primaryContact.id,
+            emails: [...emails],
+            phoneNumbers: [...phoneNumbers],
+            secondaryContactIds,
+        },
+    });
 };
